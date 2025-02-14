9a15e4814a905e4336f5a0a6729c438e3cf2de73
willykudo <72055688+willykudo@users.noreply.github.com>
2025-02-14 20:53:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(cors({
     origin: 'https://frontend-pionix.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
